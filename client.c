29b1864f71778cf26232b61de4373ce490ecffe9
Austin Herring <awh44@cs.drexel.edu>
2014-03-11 15:45:12 -0400

    Updated client.c to mostly final version. Prepared to check against already-taken usernames; to show the user the possible commands; to quit when the user decides to; to send messages to the server; and to continually read from the server. Also added the test server (test_server.c) that was used when writing the client-side.

--- client.c
+++ client.c
@@ -6,29 +6,33 @@
 #include <string.h>
 #include <pthread.h>
 
-const int MAX_STRING_SIZE = 128;
+#define BUFFER_SIZE 128
+#define PORT_NUM 9034
 
 void *get_messages(void *args);
+void print_commands();
 
 int main()
 {
+	//Establish the connection to the server--------------------------------
 	struct sockaddr_in sad;
 	int clientSocket;
+	int port = PORT_NUM;
 	struct hostent *ptrh;
 
-	char host[MAX_STRING_SIZE];
-	gethostname(host, sizeof(host));
-	strcpy(host, "tux64-13.cs.drexel.edu");
-	int port = 10001;
+	char host[BUFFER_SIZE];
+	strcpy(host, "tux64-11.cs.drexel.edu");
 
 	printf("Connecting to server...\n");
 
+	//set up the socket and sockaddr
 	clientSocket = socket(PF_INET, SOCK_STREAM, 0);
 	memset((char *) &sad, 0, sizeof(sad));
 	sad.sin_family = AF_INET;
 	sad.sin_port = htons((u_short) port);
 	ptrh = gethostbyname(host);
 	memcpy(&sad.sin_addr, (*ptrh).h_addr, (*ptrh).h_length);
+	//-------------------------------
 	
 	if (connect(clientSocket, (struct sockaddr *) &sad, sizeof(sad)) < 0)
 	{
@@ -37,24 +41,30 @@ int main()
 		return 5;
 	}
 
-	
-	printf("What would you like your username to be? (< 128 characters)\n");
-	char username[MAX_STRING_SIZE];
+	printf("Successfully connected to server.\n\n");
+	//----------------------------------------------------------------------
+
+
+	//have the user choose a username---------------------------------------
+	printf("What would you like your username to be? (No spaces, < 128 characters)\n");
+	char username[BUFFER_SIZE];
 	scanf("%s", username);
 	write(clientSocket, username, sizeof(username));
 	
-	char from_server[MAX_STRING_SIZE];
+	char from_server[BUFFER_SIZE];
 	read(clientSocket, from_server, sizeof(from_server));
 	while (strcmp(from_server, "taken") == 0)
 	{
-		printf("Sorry, that username is already taken. Please try again.\n");
+		printf("Sorry, that username is already taken. Please try another.\n");
 		scanf("%s", username);
 		write(clientSocket, username, sizeof(username));
 		read(clientSocket, from_server, sizeof(from_server));
 	}
+	//----------------------------------------------------------------------
 
-	printf("Welcome to the chatroom! You may now begin chatting. (Type \"quit\" to exit.)\n");
+	printf("\nWelcome to the chatroom, %s! You may now begin chatting. (Type \"/cmd\" to list commands.)\n", username);
 
+	//create the thread for receiving messages from the server--------------
 	pthread_t msg_thread;
 	if (pthread_create(&msg_thread, NULL, get_messages, (void *) (intptr_t) clientSocket))
 	{
@@ -62,14 +72,25 @@ int main()
 		close(clientSocket);
 		return 10;
 	}
+	//thread isn't joined because it *should* terminate when main finishes
+	//----------------------------------------------------------------------
 
-	char user_input[MAX_STRING_SIZE];
-	scanf("%s", user_input);
-	while (strcmp(user_input, "quit") != 0)
+	//get the user's messages and write them to the server------------------
+	char *user_input = (char *) malloc(BUFFER_SIZE * sizeof(char));
+	int chars_read = BUFFER_SIZE;
+	do
 	{
-		write(clientSocket, user_input, sizeof(user_input));
-		scanf("%s", user_input);
-	}
+		chars_read = getline(&user_input, &chars_read, stdin);
+		if (strcmp(user_input, "/cmd\n") == 0)
+		{
+			print_commands();
+		}
+		else
+		{
+			write(clientSocket, user_input, chars_read);
+		}
+	} while (strcmp(user_input, "/quit\n") != 0);
+	//----------------------------------------------------------------------
 
 	close(clientSocket);
 
@@ -78,8 +99,26 @@ int main()
 
 void *get_messages(void *args)
 {
+	//get the value of the shared file descriptor for the clientSocker
 	int clientSocket = (intptr_t) args;
-	char from_server[MAX_STRING_SIZE];
-	read(clientSocket, from_server, sizeof(from_server));
-	printf("%s\n", from_server);
+	//continually read from the server
+	while (1)
+	{
+		char from_server[BUFFER_SIZE + 1];
+		int bytes_read = read(clientSocket, from_server, BUFFER_SIZE);
+		if (bytes_read >= 0)
+		{
+			from_server[bytes_read] = '\0';
+		}
+		printf("%s", from_server);
+	}
+}
+
+void print_commands()
+{
+	printf("The commands are:\n");
+	printf("/roll [number] - roll die with [number] sides\n");
+	printf("/me [action] - outputs your username as performing the action\n");
+	printf("/whispter [username] [message] - send [message] only to user [username]\n");
+	printf("/quit - quit the program\n");
 }
