ab876b3cf5fd0f13606feb9b5fc8dd8bbdebb7ba
Austin Herring <awh44@drexel.edu>
2014-07-06 00:56:22 -0400

    Changed ncurses_test.c to be two windows
    Created a two-windowed version of ncurses_test. One of the windows takes input, and the other puts the output to the screen. Next step will be moving this to ncurses_client.c.

--- ncurses_test.c
+++ ncurses_test.c
@@ -8,38 +8,58 @@
 
 #define BUFF_SIZE 128
 #define BACKSPACE 7
+#define DELETE 127
 #define SPACE_VALUE 32
 #define DEL_VALUE 127
 
-int ncurses_getline(char **output, size_t *alloced);
+int ncurses_getline(char **output, size_t *alloced, WINDOW *out_win);
 
 int main(int argc, char *argv[])
 {
 	initscr();
 	cbreak();
 	keypad(stdscr, TRUE);
-	scrollok(stdscr, TRUE);
+	//scrollok(stdscr, TRUE);
 	noecho();
+
+	WINDOW *output_win = newwin(LINES - 1, COLS, 0, 0);
+	scrollok(output_win, TRUE);
+	leaveok(output_win, TRUE);
+
+	WINDOW *input_win = newwin(1, COLS, LINES - 1, 0);
+	scrollok(input_win, TRUE);
 
 	char *input = NULL;
 	size_t size = 0;
-	int chars_read = ncurses_getline(&input, &size);
+	int chars_read = ncurses_getline(&input, &size, input_win);
 
 	while (strcmp(input, "quit\n") != 0)
 	{
-		printw("%s", input);
-		refresh();
-		chars_read = ncurses_getline(&input, &size);
+		wclear(input_win);
+		wrefresh(input_win);
+		wprintw(output_win, "%s", input);
+		wrefresh(output_win);
+		chars_read = ncurses_getline(&input, &size, input_win);
 	}
 
 	free(input);
+	delwin(output_win);
+	delwin(input_win);
 
 	endwin(); //end ncurses
 
 	return 0;
 }
 
-int ncurses_getline(char **output, size_t *alloced)
+//Returns the number of characters read, including the newline
+//but ignoring the null terminator. Could potentially changed
+//the passed *output pointer by reallocing. Sets *output to
+//NULL on out of memory errors, after freeing. Note that this
+//means there will be no way to recover information from
+//user input upon out of memory errors. Note that the number
+//of characters read before running out of memory will always
+//be returned though
+int ncurses_getline(char **output, size_t *alloced, WINDOW *in_win)
 {
 	int alloc_size = *alloced;
 
@@ -50,7 +70,7 @@ int ncurses_getline(char **output, size_t *alloced)
 		*output = malloc(alloc_size * sizeof(char));
 		if (NULL == *output)
 		{
-			alloc_size = 0;
+			*alloced = 0;
 			return 0;
 		}
 	}
@@ -65,16 +85,16 @@ int ncurses_getline(char **output, size_t *alloced)
 	int needed_size = 2; //one for '\n' and one for '\0'
 
 	char input;
-	while ((input = getch()) != '\n')
+	while ((input = wgetch(in_win)) != '\n')
 	{
-		if (input == BACKSPACE)
+		if ((input == BACKSPACE) || (input == DELETE))
 		{
 			if (needed_size > 2)
 			{
 				needed_size--;
 				int x, y;
-				getyx(stdscr, y, x);
-				mvdelch(y, x - 1);
+				getyx(in_win, y, x);
+				mvwdelch(in_win, y, x - 1);
 			}
 		}
 		else if ((input >= SPACE_VALUE) && (input < DEL_VALUE))
@@ -89,21 +109,29 @@ int ncurses_getline(char **output, size_t *alloced)
 			if (needed_size > alloc_size)
 			{
 				alloc_size *= 2;
-				*output = realloc(*output, alloc_size * sizeof(char));
+				char *tmp = realloc(*output, alloc_size * sizeof(char));
+				if (NULL == tmp)
+				{
+					free(*output);
+					*alloced = 0;
+					return needed_size - 1;
+				}
+				*output = tmp;
 			}
 			(*output)[needed_size - 3] = input;
-			printw("%c", input);
+			wprintw(in_win, "%c", input);
 		}
-		refresh();
+		wrefresh(in_win);
 	}
 
 	(*output)[needed_size - 2] = '\n';
 	(*output)[needed_size - 1] = '\0';
 
-	printw("\n");
-	refresh();
+	wprintw(in_win, "\n");
+	wrefresh(in_win);
 
 	*alloced = alloc_size;
 	//subtract one for the null terminator
 	return needed_size - 1;
 }
+
